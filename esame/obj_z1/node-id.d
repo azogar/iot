343e8badb63922eddab459b40ca5c0addd16a393
unknown <ssnape@hotmail.it>
2014-05-19 15:02:37 +0200

    final

--- esame/obj_z1/node-id.d
+++
@@ -1,8 +0,0 @@
-obj_z1/node-id.o: ../../../platform/z1/./node-id.c \
- ../../../platform/z1/./node-id.h ../../../platform/z1/./contiki-conf.h \
- ../../../platform/z1/./platform-conf.h ../../../cpu/msp430/./msp430def.h \
- project-conf.h ../../../core/./dev/xmem.h
-../../../platform/z1/./node-id.c :
- ../../../platform/z1/./node-id.h ../../../platform/z1/./contiki-conf.h :
- ../../../platform/z1/./platform-conf.h ../../../cpu/msp430/./msp430def.h :
- project-conf.h ../../../core/./dev/xmem.h :
